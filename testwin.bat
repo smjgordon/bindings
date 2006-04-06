beb082ae6137599ebf7acd58fed1ae3f1f3fc0ca
Don Clugston <Don Clugston@e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd>
2006-04-06 11:56:31 +0000

    Win32 now compiles with version=Unicode, and version=WindowsXP. Added testwin.bat to easily test these configurations.
    git-svn-id: http://svn.dsource.org/projects/bindings/trunk@51 e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd

---
+++ testwin.bat
@@ -0,0 +1,5 @@
+dmd -c win32\windows.d
+dmd -version=WindowsXP -c win32\windows.d
+dmd -version=Windows2003 -c win32\windows.d
+dmd -version=Unicode -c win32\windows.d
+dmd -version=Unicode -version=Windows2003 -c win32\windows.d
