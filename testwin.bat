d280af36b1bbb0b36195411af4d45880738699d1
Don Clugston <Don Clugston@e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd>
2006-06-02 15:36:37 +0000

    Minor fixes.
    git-svn-id: http://svn.dsource.org/projects/bindings/trunk@136 e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd

--- testwin.bat
+++
@@ -1,5 +0,0 @@
-dmd -c win32\all.d
-dmd -version=WindowsXP -c win32\all.d
-dmd -version=Windows2003 -c win32\all.d
-dmd -version=Unicode -c win32\all.d
-dmd -version=Unicode -version=Windows2003 -c win32\all.d
