edc308ea76df0dba455482f2b53e26c349211b29
Trass3r <Trass3r@e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd>
2009-01-18 19:26:29 +0000

    added file with string type definitions
    git-svn-id: http://svn.dsource.org/projects/bindings/trunk@323 e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd

---
+++ lua/common.d
@@ -0,0 +1,36 @@
+﻿/*******************************************************************************
+
+	copyright:      Copyright (c) 2009 Andreas Hollandt. All rights reserved
+
+    authors:        Andreas Hollandt
+
+*******************************************************************************/
+
+module lua.common;
+
+package
+{
+// define string types for compatibility with both D1 and D2
+version (D_Version2)
+{
+	alias char[] mstring; /// mutable string type
+	alias const(char)[] cstring; /// const string type
+	alias invariant(char)[] istring; /// invariant string type
+
+	alias wchar[] mwstring;
+	alias const(wchar)[] cwstring;
+	alias invariant(wchar)[] iwstring;
+
+	alias dchar[] mdstring;
+	alias const(dchar)[] cdstring;
+	alias invariant(dchar)[] idstring;
+}
+else
+{
+/*
+	alias char[] string;
+	alias wchar[] wstring;
+	alias dchar[] dstring;
+*/
+}
+}
