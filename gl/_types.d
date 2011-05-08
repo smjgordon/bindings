7989700b4648eb068a86bd19256f47ecdb447324
hotgloupi <hotgloupi@e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd>
2011-05-08 19:45:10 +0000

    separate types to include when needed
    git-svn-id: http://svn.dsource.org/projects/bindings/trunk@366 e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd

---
+++ gl/_types.d
@@ -0,0 +1,22 @@
+
+/*
+ * Types
+ */
+alias uint	GLenum;
+alias ubyte	GLboolean;
+alias uint	GLbitfield;
+alias void	GLvoid;
+alias byte	GLbyte;
+alias short	GLshort;
+alias int	GLint;
+alias ubyte	GLubyte;
+alias ushort	GLushort;
+alias uint	GLuint;
+alias int	GLsizei;
+alias float	GLfloat;
+alias float	GLclampf;
+alias double	GLdouble;
+alias double	GLclampd;
+alias char	GLchar;
+alias ptrdiff_t	GLintptr;
+alias ptrdiff_t	GLsizeiptr;
