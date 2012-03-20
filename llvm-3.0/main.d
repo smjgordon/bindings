861969d4345901c80ff6299f0dcb3b8bec863d34
mwarning <mwarning@e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd>
2012-03-20 12:19:59 +0000

    Add example skeleton for using the bindings
    git-svn-id: http://svn.dsource.org/projects/bindings/trunk@382 e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd

---
+++ llvm-3.0/main.d
@@ -0,0 +1,11 @@
+
+import llvm.c.Core;
+
+/*
+Example for using the llvm-d bindings. - TODO
+*/
+
+void main()
+{
+    
+}
--- llvm-3.0/prebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-g++ llvm/Ext.cpp -c `llvm-config --cxxflags`
-g++ llvm/Target.cpp -c `llvm-config --cxxflags`
-
-rm -f libllvm-c-ext.a
-ar rc libllvm-c-ext.a Ext.o Target.o
-ranlib libllvm-c-ext.a
